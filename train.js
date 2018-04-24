b2c922344b17aa47b72e1c49b958e651d8bf5057
Kyle Sanders <kyle@aquaether.com>
2018-04-24 09:10:54 -0500

    basic CLI dialog

--- train.js
+++ train.js
@@ -1,16 +1,55 @@
 // basic training proof of concept: scanners
-var mysql      = require('mysql');
-	var connection = mysql.createConnection({
-	  host     : '127.0.0.1',
-	  user     : 'root',
-	  password : '',
-	  database : 'camerascrape'
-	});
+const console = require('better-console');
+const readline = require('readline');
+const mysql      = require('mysql');
+const connection = mysql.createConnection({
+  host     : '127.0.0.1',
+  user     : 'root',
+  password : '',
+  database : 'camerascrape'
+});
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
 	 
-	connection.connect();
-	connection.query('SELECT * FROM feedposts ORDER BY id DESC LIMIT 50', function (error, results, fields) {
-		connection.end();
+connection.connect();
+getItemSet();
+getFeedSet();
+
+
+
+function promptItemQuestion(item)
+{
+	rl.question("Classify: " + item.title, trainItem + " ");
+}
+
+function trainItem(answer)
+{
+	console.log("a: "+answer);
+}
+
+
+function getFeedSet()
+{
+	connection.query('SELECT id, title FROM feedposts WHERE feed_id = 4 ORDER BY id DESC LIMIT 5', function (error, res, fields) {
 		if (error)  throw error;
-		res.setHeader('Content-Type', 'application/json');
-		res.send(JSON.stringify(results));
-	});			
+		//console.log(JSON.stringify(res));
+		console.table(res);
+		res.forEach(function(item) {
+		  promptItemQuestion(item);
+		});
+		return res;
+	});				
+}
+
+function getItemSet()
+{
+	connection.query('SELECT id, name FROM items', function (error, res, fields) {
+		if (error)  throw error;
+		//console.log(JSON.stringify(res));
+		console.table(res);
+	});				
+}
+
